7c231b0633dca44c3ab25bce726e046d540f21e1
him k <him10424@gmail.com>
2018-05-04 19:07:05 +0700

    update

--- Him/Final Practice/Garage.java
+++
@@ -1,8 +0,0 @@
-public class Garage{
-	public static void main(String[] args) {
-		Car Car1 = new Car("asd", "2019", 4, 1600, "gasohol", "compact");
-		System.out.println(Car1.getSpec());
-		Car1.calDeposit(5);
-		System.out.println(Car1.getRentalRate());
-	}
-}
